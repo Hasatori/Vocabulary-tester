59df8ba76a126ffaa3b5021c6653549e106bfac1
Hasatori <hradil.o@email.cz>
2018-08-05 11:31:35 +0200

    Database connection implemented. Card layout created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
-import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { FooterComponent } from './footer/footer.component';
-
+import {AppComponent} from './app.component';
+import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
+import {FooterComponent} from './footer/footer.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
+import { CardsComponent } from './cards/cards.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CardsComponent
 
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
   ],
-  schemas: [ NO_ERRORS_SCHEMA ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
